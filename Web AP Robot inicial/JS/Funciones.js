9e02758f3ba2feaed939e6d14f55cc04ec796994
BraianCaballero <braianacaballero20gmail.com>
2024-03-18 11:53:02 -0300

    update

--- Web AP Robot inicial/JS/Funciones.js
+++ Web AP Robot inicial/JS/Funciones.js
@@ -5,20 +5,28 @@ var start = new Boolean;
 start = document.getElementById("inicio");
 const toggleBtn = document.getElementById('theme-toggle-btn');
 const body = document.body;
-const header = document.header;
+const label = document.querySelector('label');
 document.getElementById("mensaje").innerHTML = "Presionar el boton de inicio";
 
 function inicio() {
+    start = true;
     clearSquares();
     directions = [];
     cuadradoActual = 1;
     document.getElementById("mensaje").innerHTML = "";
+    if (start == true) {
+        document.getElementById("btnAvanzar").disabled = false;
+        document.getElementById("btnIzquierda").disabled = false;
+        document.getElementById("btnDerecha").disabled = false;
+        document.getElementById("btnAtras").disabled = false;
+    } 
 }
 
 function addDirection(direction) {
     if (directions.length < maxOrdenes) {
         directions.push(direction);
         showDirection(direction);
+        document.getElementById("mensaje").innerHTML = direction;
     }
     else {
         document.getElementById("mensaje").innerHTML = "Llegué a mi máximo de 10 órdenes acumuladas";
@@ -45,32 +53,31 @@ function clearSquares() {
 }
 
 function go() {
+    start = false;
     document.getElementById("mensaje").innerHTML = "¡Allá voy!";
     if (directions.length === 0) {
         document.getElementById("mensaje").innerHTML = "Presionar el boton de inicio";
         return;
     }
 
-    /*do {
+    if (start == false) {
         document.getElementById("btnAvanzar").disabled = true;
         document.getElementById("btnIzquierda").disabled = true;
         document.getElementById("btnDerecha").disabled = true;
         document.getElementById("btnAtras").disabled = true;
-    } while (!start);
-    if (!"inicio") {
-        document.getElementById("btnAvanzar").disabled = true;
-        document.getElementById("btnIzquierda").disabled = true;
-        document.getElementById("btnDerecha").disabled = true;
-        document.getElementById("btnAtras").disabled = true;
-    }*/
+    } 
 }
 
 toggleBtn.addEventListener('change', function() {
+    //Tema oscuro
     if (this.checked) {
-        body.style.backgroundColor = '#0953a1';
+        body.style.backgroundColor = '#252850';
         body.style.color = 'white';
+        label.style.backgroundColor = '#f39f18';
     } else {
+    //Tema claro
         body.style.backgroundColor = '#29bfc7';
         body.style.color = 'black';
+        label.style.backgroundColor = '#76766d ';
     }
 });
