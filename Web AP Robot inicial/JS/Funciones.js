2e7406d4c5102ff86dd62e121d934b3870d9d30b
BraianCaballero <braianacaballero20gmail.com>
2024-03-18 13:55:14 -0300

    update

--- Web AP Robot inicial/JS/Funciones.js
+++ Web AP Robot inicial/JS/Funciones.js
@@ -6,22 +6,21 @@ start = document.getElementById("inicio");
 const toggleBtn = document.getElementById('theme-toggle-btn');
 const body = document.body;
 const label = document.querySelector('label');
+const header = document.querySelector('header');
 document.getElementById("mensaje").innerHTML = "Presionar el boton de inicio";
 
+//se comienza a ejecutar esta función a partir de presionar el boton de inicio
 function inicio() {
     start = true;
     clearSquares();
     directions = [];
     cuadradoActual = 1;
     document.getElementById("mensaje").innerHTML = "";
-    if (start == true) {
-        document.getElementById("btnAvanzar").disabled = false;
-        document.getElementById("btnIzquierda").disabled = false;
-        document.getElementById("btnDerecha").disabled = false;
-        document.getElementById("btnAtras").disabled = false;
-    } 
+    habilitarBotonera(start);
 }
 
+/*esta función es la encargada de leer las direcciones escritas en el html y sumarlas 
+hasta llegar a su máximo o su ejecución, lo que ocurra primero*/
 function addDirection(direction) {
     if (directions.length < maxOrdenes) {
         directions.push(direction);
@@ -33,6 +32,8 @@ function addDirection(direction) {
     }
 }
 
+/*esta función lo que hace es agregar las flechas direccionales en los cuadrados de la cola de órdenes, 
+leyendo la dirección asignada*/
 function showDirection(direction) {
     var squares = document.getElementsByClassName("square");
     var index = directions.length -1;
@@ -44,6 +45,7 @@ function showDirection(direction) {
     }
 }
 
+//esta función limpia los cuadrados de la cola de órdenes
 function clearSquares() {
     var squares = document.getElementsByClassName("square");
 
@@ -52,32 +54,46 @@ function clearSquares() {
     }
 }
 
+function standBy() {
+    if (!inicio()) {
+        habilitarBotonera(false);
+    }
+}
+
+/*esta función se ejecuta a partir de presionar el boton ¡Vamos!*/
 function go() {
-    start = false;
-    document.getElementById("mensaje").innerHTML = "¡Allá voy!";
-    if (directions.length === 0) {
+    if (start == false  || directions.length === 0) {
         document.getElementById("mensaje").innerHTML = "Presionar el boton de inicio";
         return;
     }
-
-    if (start == false) {
-        document.getElementById("btnAvanzar").disabled = true;
-        document.getElementById("btnIzquierda").disabled = true;
-        document.getElementById("btnDerecha").disabled = true;
-        document.getElementById("btnAtras").disabled = true;
-    } 
+    start = false;
+    document.getElementById("mensaje").innerHTML = "¡Allá voy!";
+    habilitarBotonera(start);
 }
 
+//esta función habilita o deshabilita la botonera dependiendo de su condición actual
+function habilitarBotonera(condicion) {
+    if (start == condicion) {
+        document.getElementById("btnAvanzar").disabled = !condicion;
+        document.getElementById("btnIzquierda").disabled = !condicion;
+        document.getElementById("btnDerecha").disabled = !condicion;
+        document.getElementById("btnAtras").disabled = !condicion;
+    }
+}
+
+//esta función maneja el tema de visualización que se seleccione
 toggleBtn.addEventListener('change', function() {
     //Tema oscuro
     if (this.checked) {
         body.style.backgroundColor = '#252850';
         body.style.color = 'white';
-        label.style.backgroundColor = '#f39f18';
+        label.style.backgroundColor = '#60e005';
+        header.style.backgroundColor = '#11388e';
     } else {
     //Tema claro
         body.style.backgroundColor = '#29bfc7';
         body.style.color = 'black';
-        label.style.backgroundColor = '#76766d ';
+        label.style.backgroundColor = '#76766d';
+        header.style.backgroundColor = '#00ffa5';
     }
 });
